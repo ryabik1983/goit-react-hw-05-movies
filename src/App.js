d3b54d311929493d0123afa296df25d4765a4f4b
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-01-17 22:33:11 +0200

    hw5
    hw5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import  {Route, Routes} from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation';
-import MoviesPage from './components/MoviesPage/MoviesPage';
-import HomePage from './components/HomePage/HomePage';
-import MovieDetailsPage from './components/MovieDetailsPage/MovieDetailsPage';
-import NotFoundView from './components/NotFoundView/NotFoundView';
 import './App.css';
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+import LinearProgress from '@mui/material/LinearProgress';
 
-function App() {
+const Navigation = lazy(() => import('./components/Navigation/Navigation'));
+const HomePage = lazy(() => import('./components/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./components/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./components/MovieDetailsPage/MovieDetailsPage'),
+);
+
+export default function App() {
   return (
-    
     <div className="App">
-    <Navigation/>  
-    <Routes>
-    <Route path="/" element={<HomePage />} exact></Route>
-    <Route path="/movies" element={<MoviesPage />} exact></Route>
-    <Route path="/movies/: moviesId/*" element={<MovieDetailsPage />}> </Route>
-    <Route path="*" element={<NotFoundView/>} exact></Route>
+      <Suspense
+        fallback={
+          <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
+            <LinearProgress color="success" />
+          </Stack>
+        }
+      >
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} exact></Route>
+          <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route
+            path="/movies/:movieId/*"
+            element={<MovieDetailsPage />}
+          ></Route>
+          <Route path="*" element={<HomePage />}></Route>
         </Routes>
-    
+      </Suspense>
     </div>
   );
 }
-
-export default App;
